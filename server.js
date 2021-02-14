8177d6effcceefe7e2719a335ee4e6fe7c2e96be
Tim Combs <tim00combs@gmail.com>
2021-02-14 12:24:28 -0800

    fixed files again as part of my mistake in cp files from scrape hike over to here

--- server.js
+++ server.js
@@ -4,21 +4,39 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const port = 6060;
+let stuff = '';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+  axios
+    .get('https://www.mappedometer.com/?maproute=853562')
+    .then((res) => {
+      // console.log(res.data);
+      const dom = new JSDOM(res.data);
+      const theMap = dom.window.document.querySelector('#map_canvas2');
+      return theMap;
+    })
+    .then((map) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<section>hello ${map.outerHTML}</section>`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-axios
-  .get('https://www.mappedometer.com/?maproute=853562')
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// axios
+//   .get('https://www.mappedometer.com/?maproute=853562')
+//   .then((res) => {
+//     // console.log(res.data);
+//     const dom = new JSDOM(res.data);
+//     const theMap = dom.window.document.querySelector('#map_canvas2');
+//     console.log(theMap);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 server.listen(port, () => {
   console.log(`Server running at PORT:${port}/`);
