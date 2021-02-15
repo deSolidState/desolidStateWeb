7f6ee85f77477b0d6791e4aa5d5fccfa429fda63
Tim Combs <tim00combs@gmail.com>
2021-02-14 19:55:20 -0800

    added slider controls to change variables:
    - added 3 sliders
    - padding: to add a multiplicative effect to the amt of padding added
    - quality: to increase or decrease the quality of jpg screenshot
    - frames: the amt of screenshots to iterate through
    
    - changed layout
      - sliders are grouped in a container side-by-side with the image until viewport width less than 700px then it converts to a column
      - image takes up responsive width = 75% of the viewport width
    
    - TODOs:
      - figure out a way to use images that are not square
      - upload image from client computer

--- scrn_shotr/snappy.js
+++ scrn_shotr/snappy.js
@@ -2,34 +2,72 @@
 
 let count = 0;
 let frames = 500;
+let deltaQual = 0.3;
+let deltaPad = 1;
+
+// grab elements on page and add listeners
+const quality = document.getElementById('quality');
+quality.addEventListener('change', updateNum);
+
+const padFactor = document.getElementById('padding');
+padFactor.addEventListener('change', updateNum);
 
 const mult = document.getElementById('mult');
 mult.addEventListener('click', takeMultScreenShots);
+
+function updateNum(e) {
+  let numSpot = e.target.previousSibling.previousSibling;
+  let text = numSpot.innerHTML;
+
+  let numStr = text.slice(-4);
+  let beginStr = text.slice(0, -4);
+
+  numSpot.innerHTML = beginStr + fourDigitStr(e.target.value);
+
+  switch (e.target.name) {
+    case 'quality':
+      deltaQual = e.target.value / 100;
+      break;
+    case 'padding':
+      deltaPad = Math.floor(e.target.value / 2);
+      break;
+    case 'frames':
+      frames = e.target.value;
+      console.log(frames);
+      break;
+    default:
+      console.log('you have a problem with the code');
+  }
+}
 
 function fourDigitStr(num) {
   let str = '';
   let numDigits = 4;
   // https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
-  const len = numDigits - ((Math.log(num) * Math.LOG10E + 1) | 0); // num of digits for any positive integer
+  let len = numDigits - ((Math.log(num) * Math.LOG10E + 1) | 0); // num of digits for any positive integer
+  len = len === 4 ? 3 : len;
+  console.log('len', len);
 
   for (let i = 0; i < len; i++) {
     str = str + '0';
   }
+
   str = str + num.toString();
 
   return str;
 }
 
-function getPaddingAmts() {
+function getPaddingAmts(factor) {
   let padStrArr = [];
 
   // creating random px amounts to add to padding to image
   for (let i = 0; i < 4; i++) {
-    let amt = Math.floor(Math.random() * 4) + 1;
+    let amt = factor * Math.floor(Math.random() * 4) + 1;
     // amt = i % 2 === 1 ? amt : amt - 2;
     // console.log(i, 'i', amt, 'amt');
     padStrArr.push(amt.toString() + 'px');
   }
+  console.log(padStrArr);
   return padStrArr;
 }
 
@@ -44,17 +82,18 @@ function takeMultScreenShots() {
     console.log(frames, 'frames');
     html2canvas(document.getElementById('img-bucket'), {
       onrendered: function (canvas) {
-        let padding = getPaddingAmts();
+        let padding = getPaddingAmts(deltaPad);
 
         // default is png which is not lossy
         // webp and jpg are lossy, so 2nd argument is the image quality
         // 0.92 is default
         // let time = Date.now();
         // let deltaQual = Math.sin(Date.now());
-        let deltaQual = 0.3;
+        // let deltaQual = 0.3;
         // deltaQual = deltaQual < 0 ? Math.cos(time) : deltaQual;
         console.log('deltaQ', deltaQual);
         var img = canvas.toDataURL('image/jpeg', deltaQual);
+        console.log(padding);
         $('#result-image')
           .attr('src', img)
           .css(
@@ -72,7 +111,7 @@ function takeMultScreenShots() {
       allowTaint: true,
       imageTimeout: 0,
       useCORS: true,
-      logging: true,
+      // logging: true,
     });
     frames = frames - 1;
     count = count + 1;
